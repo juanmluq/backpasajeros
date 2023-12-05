b8ca6e5f48fbc1e53011fec2a2d8f4161bd569d3
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2023-12-05 03:11:50 -0300

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ const server = require('./src/app.js');
 const { conn } = require('./src/db.js');
 const port = process.env.PORT || 3001;
 // Syncing all the models at once.
-conn.sync({ force: false }).then(() => {// el force true dice que cada vez que corto el back borra la base de datos y cuando levanto vuelve a crear la base de datos
+conn.sync({ force: true }).then(() => {// el force true dice que cada vez que corto el back borra la base de datos y cuando levanto vuelve a crear la base de datos
   server.listen(port, () => {
     console.log('%s listening at 3001'); // eslint-disable-line no-console
   });
