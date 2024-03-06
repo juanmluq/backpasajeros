f0210fb92e9cda7128ce56a852bdc4cb43582bb8
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-03-06 20:11:42 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,7 @@
 const { Router } = require('express');
-//const { path } = require('../app');
 const axios = require('axios');
 const {Pasajero} = require ("../db");
-// Importar todos los routers;
-// Ejemplo: const authRouter = require('./auth.js');
-
 const router = Router();
-
-const users = [
-  {id: 1, name: 'Franco', email: 'Franco@gmail.com', password: '1234'},
-  {id: 2, name: 'Matias', email: 'Matias@gmail.com', password: '1234'}
-]
 
 const isAuthenticated = (req, res, next) => {
   const { userId } = req.cookies;
@@ -26,12 +17,6 @@ const isNotAuthenticated = (req, res, next) => {
   next();
 };
 
-
-
-// Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);
-//const YOUR_API_KEY = a256b5f8e0754e568482f7d8ef4cf6d8;
-
 const getDbInfo = async () => {// esta funcion me trae la info de la base de datos
     return await Pasajero.findAll({})
 }
@@ -39,23 +24,21 @@ const getDbInfo = async () => {// esta funcion me trae la info de la base de dat
 const getAllPasajeros = async () => { //aca JUNTO LA DB Y LA INFO DE LA API
     const dbInfo = await getDbInfo();
     return dbInfo
-}
-
-          
+}     
 
 router.get("/pasajeros", async (req, res) => {
     const name = req.query.name
     let pasajerosTotal = await getAllPasajeros();
-    if(name) {//si hay un name que me pasen por query
+      if(name) {//si hay un name que me pasen por query
         let pasajeroName = await pasajerosTotal.filter(el => el.name.toLowerCase().includes(name.toLowerCase()))//agarra el name y fijate si incluye lo
         // que le pase por query en este caso name. tambien convierte a minuscula para comparar que sea igual indistinto si hay mayusculas. El include es 
         // para hacer una busqueda mas global (si pongo === deberia ser exactamente lo mismo).
         pasajeroName.length ? //si existe videogameName devolver ese videogameName:
         res.status(200).send(pasajeroName) : //sino hacer:
         res.status(404).send("No esta el pasajero");
-    } else { //si no me pasan name por query hacer:
+      } else { //si no me pasan name por query hacer:
         res.status(200).send(pasajerosTotal)
-    }
+      }
     })
 
 
@@ -70,7 +53,7 @@ router.get("/pasajeros", async (req, res) => {
         cantpersonas,
         cantnoches,
         pedidoadic,
-        createdindb //esto es para ver si esta creado en db
+        createdindb
     } = req.body
  
 
@@ -84,7 +67,7 @@ router.get("/pasajeros", async (req, res) => {
       cantpersonas,
       cantnoches,
       pedidoadic,
-      createdindb //no se pone genres porque es una relacion aparte
+      createdindb 
     })
    
   console.log(pasajeroCreated); 
@@ -102,8 +85,7 @@ router.get("/pasajeros", async (req, res) => {
     }
 });
 
-router.delete("/pasajeros/:id", async (req,res) => {
-    
+router.delete("/pasajeros/:id", async (req,res) => {  
   try{
       let id = req.params.id;
       await Pasajero.destroy({
