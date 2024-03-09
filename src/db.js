883ab3c728e7bf31d6bcee28f752c6792792e118
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2024-03-09 14:25:00 -0300

    Update db.js

--- src/db.js
+++ src/db.js
@@ -10,12 +10,11 @@ const sequelize = new Sequelize(`postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOS
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
 });
-//aca guardamos la direccion de este archivo basename = C:\Users\Yo que se\Desktop\PI-Videogames-main\api\src\db.js
+//aca guardamos la direccion de este archivo basename
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
 // Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
-//__dirname = C:\Users\Yo que se\Desktop\PI-Videogames-main\api\src
 //aca juntamos dirname con /models y leemos todos los modelos que hay en la carpeta. En este caso son 2
 fs.readdirSync(path.join(__dirname, '/models'))
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
@@ -33,15 +32,7 @@ let entries = Object.entries(sequelize.models);
 let capsEntries = entries.map((entry) => [entry[0][0].toUpperCase() + entry[0].slice(1), entry[1]]);
 sequelize.models = Object.fromEntries(capsEntries);
 
-// En sequelize.models están todos los modelos importados como propiedades
-// Para relacionarlos hacemos un destructuring
 const { Pasajero } = sequelize.models;
-
-// Aca vendrian las relaciones
-// Product.hasMany(Reviews);
-//un genero de mi tabla de genero pertenece a muchos videogames de mi tabla de videogames
-//un videogame de mi tabla de videogame pertenece a muchos generos de mi tabla de generos
-//haciendo esto, estoy creando una tabla intermedia en db con un id perteneciente al genero y un id perteneciente al videogame 
 
 module.exports = {
   ...sequelize.models, // para poder importar los modelos así: const { Product, User } = require('./db.js');
