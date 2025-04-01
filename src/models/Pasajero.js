ca710729e501fcb78ac21a1255bbd7f5a9d71791
Juan Manuel Luque <94568781+juanmluq@users.noreply.github.com>
2025-03-31 22:47:45 -0300

    Update Pasajero.js

--- src/models/Pasajero.js
+++ src/models/Pasajero.js
@@ -1,22 +1,19 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('pasajero', {
     id: {
-      type: DataTypes.UUID, //es un numero random con numeros y letras que no se va a repetir. Porq cuando mando los datos de la api asi no se van a pisar los id
+      type: DataTypes.UUID,
       defaultValue: DataTypes.UUIDV4, //
-      allowNull: false, //allownull dice que no te permito que no este en vacio. si le pongo false le digo que no este vacio.
-      primaryKey: true //es la clave primaria osea el id.
+      allowNull: false, 
+      primaryKey: true 
     },
     name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(50),
       allowNull: false,
-      //unique: esto me sirve para un mail por ejemplo. suponiendo que registro mi mail luque11@gmail.com si viene otro a querer regristrarse con el mismo mail no lo deja
     },
     email: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(60),
       allowNull: false,
       unique: false
     },
@@ -24,16 +21,16 @@ module.exports = (sequelize) => {
       type: DataTypes.INTEGER,
       allowNull: false
     },
-    nacimiento: {//comparar con el pi breadkingbad hay que poner DataTypes.ENUM(...)
-      type: DataTypes.STRING,
+    nacimiento: {
+      type: DataTypes.STRING(10),
       allowNull: true
     },
     fecsalida: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(10),
       allowNull: true,
     },
     destino: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(60),
       allowNull: true
     },
     cantpersonas: {
